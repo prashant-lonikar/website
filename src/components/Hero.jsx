ad0aac58b41f8af173a07084fd133a13ea0e382c
Prashant Lonikar <prashant@climatealigned.co>
2023-11-05 21:34:21 +0000

    Added more personalisations

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,22 +5,22 @@ const Hero = () => {
   return (
     <div className='text-white'>
         <div className='max-w-[800px] mt-[96px] w-full h-screen mx-auto text-center flex flex-col justify-center'>
-            <p className='text-[#00df9a] font-bold p-2'>GROWING WITH DATA ANALYTICS</p>
-            <h1 className='md:text-7xl sm:text-6xl text-4xl font-bold md:py-6'>Grow with data.</h1>
+            <p className='text-[#00df9a] font-bold p-2 uppercase'>Curriculum vitae</p>
+            <h1 className='md:text-7xl sm:text-6xl text-4xl font-bold md:py-6'>Prashant Lonikar</h1>
             <div className='flex justify-center items-center'>
                 <p className='md:text-5xl sm:text-4xl text-xl font-bold py-4'>
-                    Fast, flexible financing for
+                    Chat about Prashant's 
                 </p>
                 <Typed 
                     className='md:text-5xl sm:text-4xl text-xl font-bold md:pl-4 pl-2'
-                    strings={['BTB', 'BTC', 'SASS']} 
-                    typeSpeed={120} 
-                    backSpeed={140} 
+                    strings={['projects', 'roles', 'hobbies']} 
+                    typeSpeed={100} 
+                    backSpeed={40} 
                     loop
                 />
             </div>
-            <p className='md:text-2xl text-xl font-bold text-gray-500'>Monitor your data analytics to increase revenue for BTB, BTC, & SASS platforms.</p>
-            <button className='bg-[#00df9a] w-[200px] rounded-md font-medium my-6 mx-auto py-3 text-black'>Get Started</button>
+            <p className='md:text-2xl text-xl font-bold text-gray-500'>Why look at a CV when you can talk to it!</p>
+            <button className='bg-[#00df9a] w-[200px] rounded-md font-medium my-6 mx-auto py-3 text-black'>Start Chatting!</button>
         </div>    
     </div>
   )
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,7 +10,7 @@ const Navbar = () => {
 
   return (
     <div className='flex justify-between items-center h-24 max-w-[1240px] mx-auto px-4 text-white'>
-        <h1 className='w-full top-0 text-3xl font-bold text-[#00df9a]'>Prashant Lonikar</h1>
+        <h1 className='w-full top-0 text-3xl font-bold text-[#00df9a]'>PL.</h1>
         <ul className='hidden md:flex'>
             <li className='p-4'>Home</li>
             <li className='p-4'>Chat</li>
