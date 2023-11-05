874672506316797f2f9529de609dba7516ecffd0
Prashant Lonikar <prashant@climatealigned.co>
2023-11-05 08:46:28 +0000

    Changed basic names

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,24 +10,24 @@ const Navbar = () => {
 
   return (
     <div className='flex justify-between items-center h-24 max-w-[1240px] mx-auto px-4 text-white'>
-        <h1 className='w-full text-3xl font-bold text-[#00df9a]'>REACT.</h1>
+        <h1 className='w-full top-0 text-3xl font-bold text-[#00df9a]'>Prashant Lonikar</h1>
         <ul className='hidden md:flex'>
             <li className='p-4'>Home</li>
-            <li className='p-4'>Company</li>
-            <li className='p-4'>Resources</li>
-            <li className='p-4'>About</li>
+            <li className='p-4'>Chat</li>
+            <li className='p-4'>Projects</li>
+            <li className='p-4'>CV</li>
             <li className='p-4'>Contact</li>
         </ul>
         <div onClick={handleNav} className='block md:hidden'>
           {nav ? <AiOutlineClose size={20}/> :<AiOutlineMenu size={20}/>}
         </div>
-        <div className={nav ? 'fixed left-0 top-0 w-[60%] h-full border-r border-r-gray-900 bg-[#000300] ease-in-out duration-500' : 'fixed left-[-100%]'}>
-        <h1 className='w-full text-3xl font-bold text-[#00df9a] m-4'>REACT.</h1>
+        <div className={nav ? 'fixed left-0 top-0 w-[60%] h-full border-r border-r-gray-900 bg-[#000300] ease-in-out duration-500 bg-opacity-80 backdrop-blur-sm' : 'fixed left-[-100%]'}>
+        <h1 className='w-full text-3xl font-bold text-[#00df9a] m-4'>Menu</h1>
         <ul className='uppercase p-4'>
           <li className='p-4 border-b border-gray-600'>Home</li>
-          <li className='p-4 border-b border-gray-600'>Company</li>
-          <li className='p-4 border-b border-gray-600'>Resources</li>
-          <li className='p-4 border-b border-gray-600'>About</li>
+          <li className='p-4 border-b border-gray-600'>Chat</li>
+          <li className='p-4 border-b border-gray-600'>Projects</li>
+          <li className='p-4 border-b border-gray-600'>CV</li>
           <li className='p-4'>Contact</li>
         </ul>
         </div>
